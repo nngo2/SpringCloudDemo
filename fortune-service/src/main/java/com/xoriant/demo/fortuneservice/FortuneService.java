bda49b2133776987e2426014d233ff8674d370d6
namngo <hnnngo@mum.edu>
2018-09-28 15:22:43 -0500

    Test GitHub polling

--- fortune-service/src/main/java/com/xoriant/demo/fortuneservice/FortuneService.java
+++ fortune-service/src/main/java/com/xoriant/demo/fortuneservice/FortuneService.java
@@ -10,7 +10,8 @@ public class FortuneService {
 	private static final String[] Fortunes = { "You learn from your mistakes... You will learn a lot today.",
 			"You can always find happiness at work on Friday", "You will be hungry again in one hour.",
 			"Today will be an awesome day!", "Good news is waiting for you next month", "It is the right time to commence your plan", 
-			"Your future is yours", "More challenges are coming, stay tune!", "Someone is The more you travel, the more you learn..."};
+			"Your future is yours", "More challenges are coming, stay tune!", "The next Monday day is your day!",
+			"The more you travel, the more you learn..."};
 
 	private Random random = new Random();
 
